be54a34a64c5cc9ad88df03ae644428bc349f281
sepideh zamanvaziri <zamanvaziri.sepideh@gmail.com>
2020-09-09 22:40:56 +0000

    implement loadTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -28,46 +28,58 @@ $(document).ready( () => {
     return $tweet;
   };
   const renderTweets = function(tweets) {
+    $('#tweets-container').empty();
     for (let tweet of tweets) {  // loops through tweets
       let $tweet = createTweetElement(tweet);  // calls createTweetElement for each tweet
       $('#tweets-container').append($tweet);  // takes return value and appends it to the tweets container
     }
   };
   // Test / driver code (temporary). 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  renderTweets(data);
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
+  // renderTweets(data);
   
   $('.new-tweet form').submit( function(evt) {
     evt.preventDefault();
     //console.log(evt);
-    console.log(typeof($(this).serialize()));
+    //console.log(typeof($(this).serialize()));
+    //console.log($(this).serialize());
     $.post('/tweets', $( this ).serialize()).done( function(data) {
-      $('#tweet-text').empty();
+      $('#tweet-text').val('');
+      $('.counter').val(140);
       console.log('tweet posted!');
-    })
+    }) 
+    loadTweets();
   })
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function (response) {
+      console.log('get request was successful');
+      renderTweets(response);
+    });
+  }
+  loadTweets();
   
 })
