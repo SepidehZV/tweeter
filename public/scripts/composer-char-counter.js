a0a33fee2d7a569dc67ec8976ab36b23e256992b
sepideh zamanvaziri <zamanvaziri.sepideh@gmail.com>
2020-09-09 16:36:10 +0000

    correct counter color

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('.new-tweet textarea').on('keyup', function() {
 
     let counter = 140 - $(this).val().length;
-    $(this).siblings('div').children('.counter').text(counter);
+    $(this).siblings('div').children('.counter').text(counter).css('color', 'black');
     if (counter < 0) {
       $(this).siblings('div').children('.counter').css('color', 'red');
     }
